9cbb203bb2414ee0488d8cbffe2766596d5363b8
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-05-24 11:59:27 -0500

    added multiple pages after wireframes were created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from "react"; 
 import logo from './logo.svg';
+import Home from "./Pages/Home"
+import {BrowserRouter, Route} from "react-router-dom"
+import { Nav, Navbar} from "react-bootstrap"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h2> Template for Primo Website </h2>
-        <p> wireframing day - 1 </p>
-      </header>
-    </div>
-  );
+class App extends Component{
+constructor(props){
+  super(props)
+  this.state = {
+  }
 }
+
+
+render() {
+  return(
+<div className="browseBox">
+    <BrowserRouter>
+      <div className="navBox">
+        <Navbar bg="dark" variant="dark">
+            <Nav className="mr-auto">
+              <Nav.link href="/">Home</Nav.link>
+              <Nav.link href="/products"> Products </Nav.link>
+              <Nav.Link href="/blog"> Blog </Nav.Link>
+            </Nav>        
+        </Navbar>
+          <Route path="/" exact render={() => <Home Home={Home} /> } /> 
+      </div>
+    </BrowserRouter>
+  </div>
+  )
+    
+    
+  }
+}
+
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react"; 
+
+class Home extends Component {
+   constructor(props){
+      super(props)
+      this.state = {
+      }
+   }
+   render(){
+      return(
+         <div className="homeBox">
+            <h1> Primo </h1>
+            <p> Story Of You </p> 
+         </div>
+      )
+   }
+}
+
+export default Home;  
