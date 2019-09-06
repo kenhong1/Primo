1da336e2dab4f74e0ff3209be62889111bb5f489
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-09-06 11:37:54 -0500

    saving local changes

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -7,7 +7,8 @@ const Products = (props) => {
       return(
          <div className="productsBox">
             <br /> 
-          
+               <h1> This is your products page </h1>
+               <p> products shows up here </p>
 
          </div>
       )
